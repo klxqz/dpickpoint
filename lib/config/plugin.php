a68dc6d8f0d911b8cdf7370c6cd78a7f6cfbc8d8
klxqz <klxqz@mail.ru>
2013-11-22 14:54:25 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,9 +1,10 @@
 <?php
+
 return array(
-    'name'        => 'Pick Point',
+    'name' => 'PickPoint',
     'description' => 'Расчет стоимости доставки через http://pickpoint.ru/',
-    'icon'        => 'img/dpickpoint16.png',
-    'logo'        => 'img/dpickpoint.png',
-    'version'     => '1.0.0',
-    'vendor'      => 903438,
+    'icon' => 'img/dpickpoint16.png',
+    'logo' => 'img/dpickpoint.png',
+    'version' => '1.0.1',
+    'vendor' => '985310',
 );
