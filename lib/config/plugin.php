7e347d2e3e7f367fa1334edd928bf86b2a25e1be
kl <kl@10.66.3.70>
2014-09-22 11:25:53 +0600

    v1.0.2

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -5,6 +5,6 @@ return array(
     'description' => 'Расчет стоимости доставки через http://pickpoint.ru/',
     'icon' => 'img/dpickpoint16.png',
     'logo' => 'img/dpickpoint.png',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'vendor' => '985310',
 );
