e479d3f29c58822d17b12e0b08f1b7fa1f4226c9
admin <admin@xstream>
2015-07-29 11:19:33 +0600

    поддержка протокола https

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -5,6 +5,6 @@ return array(
     'description' => 'Расчет стоимости доставки через http://pickpoint.ru/',
     'icon' => 'img/dpickpoint16.png',
     'logo' => 'img/dpickpoint.png',
-    'version' => '1.2.0',
+    'version' => '1.2.1',
     'vendor' => '985310',
 );
